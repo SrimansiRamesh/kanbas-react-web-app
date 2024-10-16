10a23ddc389ee5a620a1e47d25b100d8da8d9c2b
SrimansiRamesh <srimansi1307@gmail.com>
2024-10-16 18:32:58 -0400

    assignment allignment issues

--- src/Kanbas/Courses/Assignments/AssignmentControls.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControls.tsx
@@ -1,50 +1,27 @@
-// import { FaPlus } from "react-icons/fa";
-// import { MdOutlineCancel } from "react-icons/md";
-// import GreenCheckmark from "../Modules/GreenCheckmark";
-// import { GoPlus } from "react-icons/go";
-// import { IoIosSearch } from "react-icons/io";
-// import { BsPlus } from "react-icons/bs";
-// import { IoEllipsisVertical } from "react-icons/io5";
-
-// export default function AssignmentControls(){
-//     return(
-//         <div className="float-end">
-//             <GreenCheckmark />
-//             <IoEllipsisVertical className="fs-4" />
-//         </div>
-//     );
-// }
-
-
 import { FaPlus } from "react-icons/fa";
 import { MdOutlineCancel } from "react-icons/md";
 import GreenCheckmark from "../Modules/GreenCheckmark";
 import { GoPlus } from "react-icons/go";
 import { IoIosSearch } from "react-icons/io";
 
-export default function AssignmentControls(){
-    return(
-        <div id="wd-modules-controls" className="text-nowrap group-icons">
-        <button id="wd-add-module-btn" className="btn btn-md btn-danger me-1 float-end">
-            <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-            Assignment
+export default function AssignmentControls() {
+    return (
+      <div id="wd-modules-controls" className="text-nowrap">
+        <button
+          id="wd-add-module-btn"
+          className="btn btn-md btn-danger me-1 float-end"
+        >
+          <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+          Assignment
         </button>
-        <button id="wd-view-progress" className="btn btn-md me-1 btn-secondary float-end position-relative me-2" style={{ bottom: "1px" }}>
-            <GoPlus />
-            Group
+        <button
+          id="wd-add-module-btn"
+          className="btn btn-md btn-secondary me-1 float-end"
+        >
+          <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+          Group
         </button>
-        <div className="position-relative" style={{ display: 'inline-block', marginLeft:"15px" }}>
-                <span className="position-absolute" style={{ top: '50%', left: '10px', transform: 'translateY(-50%)' }}>
-                    <IoIosSearch />
-                </span>
-                <input 
-                    type="text" 
-                    id="wd-search-input" 
-                    className="form-control ps-5 border border-secondary" 
-                    placeholder="Search..." 
-                    style={{ width: '300px'}}
-                />
-            </div>
-        </div>
+        
+      </div>
     );
-}
+  }
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,60 +1,82 @@
-
-import { BsGripVertical } from "react-icons/bs";
-import ModuleControlButtons from "../Modules/ModuleControlButtons";
-import AssignmentControls from "./AssignmentControls";
-import AssignmentControlButtons from "./AssignmentControlButtons";
-import { GoTriangleDown } from "react-icons/go";
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import * as db from '../../Databases';
+import AssignmentControls from './AssignmentControls';
+import { FaSearch, FaPlus } from 'react-icons/fa';
+import LessonControlButtons from '../Modules/LessonControlButtons';
+import { IoEllipsisVertical } from 'react-icons/io5';
+import { BsGripVertical } from 'react-icons/bs';
 import { GiNotebook } from "react-icons/gi";
-import GreenCheckmark from "../Modules/GreenCheckmark";
-import { IoEllipsisVertical } from "react-icons/io5";
-import * as db from "../../Databases";
-import { useParams } from "react-router";
+
 export default function Assignments() {
-  const { cid } = useParams();
-  const assignments = db.assignments.filter(assignment => assignment.course === cid);
-    return (
-      <div id="wd-assignments" className="container-fluid my-4">
-        <div className="d-flex flex-column w-100">
-        <AssignmentControls /><br/><br/>
-        <ul id="wd-assignments" className="list-group rounded-0 module-container w-100" style={{overflowX:'hidden'}}>
-          <li className="wd-assignment list-group-item p-0 mb-5 fs-5 border-gray w-100">
-            <div className="wd-title p-3 ps-2 bg-light d-flex align-items-center w-100">
-                  <BsGripVertical className="me-2 fs-3" />
-                  <GoTriangleDown className="me-2 fs-4"/>
-                    Assignments
-                    <div className="ms-auto">
-                      <AssignmentControlButtons />
-                    </div>
+  const { cid } = useParams(); // Retrieve courseId from route params
+  const assignments = db.assignments;
+  const courseAssignments = assignments.filter(
+    (assignment) => assignment.course === cid
+  );
+  return (
+    <div className="w-100 p-5">
+      <div className="d-flex justify-content-between align-items-center my-3">
+        <div className="input-group" style={{ width: "300px" }}>
+          <span className="input-group-text" id="basic-addon1">
+            <FaSearch />
+          </span>
+          <input
+            id="wd-search-assignment"
+            type="text"
+            className="form-control"
+            placeholder="Search for Assignments"
+          />
+        </div>
+        <AssignmentControls />
+      </div>
+      <br />
+      <ul className="list-group rounded-0">
+        <li className="wd-assignment-group list-group-item p-0 fs-5 border-gray">
+          <div className="wd-title p-3 d-flex justify-content-between align-items-center bg-secondary">
+            <div className="d-flex align-items-center">
+              <BsGripVertical className="me-2 fs-3" />
+              <span className="ms-2">Week 1 Assignments</span>
             </div>
-            <ul className="wd-lessons list-group rounded-0 w-100">
-            {assignments.map(assignment => (
-              <li key={assignment._id} className="wd-lesson d-flex border-bottom border-gray w-100 style={{ overflowX: 'hidden' }}">
-                <div className="d-flex align-items-center">
-                  <BsGripVertical className="ms-2 fs-4" />
-                  <GiNotebook className="ms-3 text-success fs-4"/>
-                </div>
-                <div className="fs-6 assignment-padding w-100">
-                  <a className="wd-assignment-link text-decoration-none text-dark ms-3"
-                    href={`#/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}>
-                  <strong>{assignment.title}</strong><br/>
-                  </a>
-                  <div className="ms-3 text-muted">
-                  <span className="text-danger">{assignment.title} </span>| <span className="text-dark">Not available until</span> {assignment.availableDate} |<br/><span className="text-dark">Due</span> {assignment.dueDate}| {assignment.points} pts
-                  </div>
-                </div>
-                <div className="ms-auto d-flex align-items-center">
-                <span className="me-4"> 
-                  <GreenCheckmark />
-                </span>
-                  <IoEllipsisVertical className="me-4"/>
-                </div>
-              </li>
-              ))}
-            </ul>
-          </li>
-        </ul>
-      </div>
-      </div>
-      
-  );}
-  
+            <div className="d-flex align-items-center">
+              <span className="text-muted me-4 border rounded-pill border-black p-2">
+                40% of Total
+              </span>
+              <FaPlus className="me-2" aria-label="Add Assignment" />
+              <IoEllipsisVertical className="fs-4" aria-label="More options" />
+            </div>
+          </div>
+        </li>
+        {courseAssignments.length > 0 ? (
+          courseAssignments.map((assignment) => (
+            <li
+              key={assignment._id}
+              className="list-group-item p-3 d-flex align-items-center"
+              style={{ borderLeft: "4px solid green" }}
+            >
+              <BsGripVertical className="me-3 fs-2" />
+              <GiNotebook className="me-3 text-success" style={{fontSize:'2rem'}}/>
+              <div className="flex-grow-1">
+                <Link
+                  to={`/courses/${cid}/Assignments/${assignment._id}`}
+                  className="text-decoration-none text-dark"
+                >
+                  <span className="fw-bold fs-5">{assignment.title}</span>
+                </Link>
+                <br />
+                <small className="text-muted">
+                  <span className="text-danger">{assignment.title}</span> |{" "}
+                  <b>Not available until</b> {assignment.availableDate} at 12:00 am | <br />
+                  <b>Due</b> {assignment.dueDate} at 11:59pm | {assignment.points} pts
+                </small>
+              </div>
+              <LessonControlButtons />
+            </li>
+          ))
+        ) : (
+          <p>No assignments available for this course.</p>
+        )}
+      </ul>
+    </div>
+  );
+}
