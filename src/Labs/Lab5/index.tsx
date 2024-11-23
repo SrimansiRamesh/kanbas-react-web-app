9c567cff7ae8d9294ab96c22c80d2b4ad24726c3
SrimansiRamesh <srimansi1307@gmail.com>
2024-11-22 23:47:31 -0500

    a5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -6,9 +6,9 @@ import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsync";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsync";
-const REMOTE_SERVER = process.env.REMOTE_SERVER;
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
-  
+
     return (
       <div id="wd-lab5">
         <h2>Lab 5</h2>
