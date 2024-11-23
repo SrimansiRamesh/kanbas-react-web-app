f7e552d49988f8c7d7574fcb63af7979b8b269d0
SrimansiRamesh <srimansi1307@gmail.com>
2024-11-22 21:50:29 -0500

    a5

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -8,6 +8,7 @@ export const fetchAllCourses = async () => {
 
 export const deleteCourse = async (id: string) => {
   const { data } = await axios.delete(`${COURSES_API}/${id}`);
+  console.log(data);
   return data;
 };
 
