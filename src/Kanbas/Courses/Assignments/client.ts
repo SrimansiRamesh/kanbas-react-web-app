48003187357d393b411159b87ba913f6873263c9
SrimansiRamesh <srimansi1307@gmail.com>
2024-11-22 23:17:44 -0500

    a5

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER || "http://localhost:4000";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 export const findAssignmentsForCourse = async (courseId: string): Promise<any[]> => {
