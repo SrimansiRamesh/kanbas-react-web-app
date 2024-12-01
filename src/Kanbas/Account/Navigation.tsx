de87b2d62da9e94de85e00e6524bb74924065525
SrimansiRamesh <srimansi1307@gmail.com>
2024-12-01 16:49:24 -0500

    a6 react app

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,6 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
@@ -20,7 +19,27 @@ export default function AccountNavigation() {
                 className={`${pathname.includes("Signup")?"list-group-item active border border-0":"list-group-item text-danger border border-0"}`}>Sign Up</Link><br/>  
             </>
           )}
+
+        {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kanbas/Account/Users`} className={`${pathname.includes("Users")?"list-group-item active border border-0":"list-group-item text-danger border border-0"}`}> Users </Link> )}
+
       </div>
 );}
 
 
+// import { Link, useLocation } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// export default function AccountNavigation() {
+//  const { currentUser } = useSelector((state: any) => state.accountReducer);
+//  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+//  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+//  const { pathname } = useLocation();
+//  return (
+//    <div id="wd-account-navigation" className="list-group">
+//      {links.map((link) => (
+//        <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}> {link} </Link>
+//      ))}
+//      {currentUser && currentUser.role === "ADMIN" && (
+//        <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
+//    </div>
+// );}
