98b98faf9c4f6d23182908b075619b9bc6a47107
SrimansiRamesh <srimansi1307@gmail.com>
2024-12-05 14:41:41 -0500

    server  connection

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router";
 export default function TOC() {
   const { pathname } = useLocation();
+  /*Test comment */
   return (
     <ul className="nav nav-pills">
       <li className="nav-item">
